bb638cadc3e79bac830b94f7c9ba32d57823610f
alanbebermartins <abebermartins@gmail.com>
2024-05-31 16:50:25 -0300

    Loop for que imprime os numeros de 1 até 100

--- numeros_ate_cem.c
+++ numeros_ate_cem.c
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 #include <locale.h>
 
+// Loop for 
+
 int main(){
     for (int i = 0; i <= 100; i++){
         printf("Imprimindo o numero %d de 100\n", i);
     }
 }
+
+// Loop while 
+int main(){
+    int i = 0;
+    while (i <= 100){
+        printf("Imprimindo o numero %d de 100\n", i);
+        i++;
+    }
+}
+
