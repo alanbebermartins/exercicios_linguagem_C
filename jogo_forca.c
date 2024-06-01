4b944642bbd3fba5fbf7b2885dd8416be01cc86f
alanbebermartins <abebermartins@gmail.com>
2024-06-01 16:39:17 -0300

    Jogo da Forca

---
+++ jogo_forca.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+int main(){
+
+    printf("************************************\n");
+    printf("*****Bem vindo ao jogo da forca*****\n");
+    printf("************************************\n");
+
+}
