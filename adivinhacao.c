4bdb5200ea9b20b0e1b1b08331ed861f736bd1ce
alanbebermartins <abebermartins@gmail.com>
2024-06-01 15:56:03 -0300

    Jogo do numero secreto

--- adivinhacao.c
+++ adivinhacao.c
@@ -4,25 +4,22 @@
 #include <time.h>
 
 int main(){
-    // Imprime o cabecalho do jogo
+
     printf("***************************************\n");
     printf("* Bem vindo ao jogo do número secreto *\n");
     printf("***************************************\n");
 
-    // A palavra reservada int serve para tipar a variavel do tipo inteiro
     int segundos = time(0);
     srand(segundos);
-
     int numerorandomico = rand();
     int numeroSecreto = numerorandomico % 100;
     int chute;
     int ganhou = 0;
     int tentativas = 0;
     double pontos = 1000;
+    
     while (ganhou == 0){
         printf("Informe qual é o número secreto!\n");
-        // printf("O numero %d e o secreto!\n", numeroSecreto);
-        // a função scanf serve para ler o que foi digitado no teclado, e recebe 2 parametros, e obrigatoriamente precisa do & comercial antes da variaviavel que está lendo
         scanf("%d", &chute);
         printf("O seu chute foi %d!\n", chute);
         if (chute < 0){
