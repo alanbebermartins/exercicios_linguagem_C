abd8a3829cf109b721f4820f73d05d70a23009f8
alanbebermartins <abebermartins@gmail.com>
2024-06-05 16:11:42 -0300

    Jogo forca atualizado

--- jogo_forca.c
+++ jogo_forca.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <time.h>
+#include <string.h>
 
 int main(){
 
@@ -9,4 +7,48 @@ int main(){
     printf("*****Bem vindo ao jogo da forca*****\n");
     printf("************************************\n");
 
+    char palavrasecreta[20];
+
+    sprintf(palavrasecreta, "MELANCIA");
+    int acertou = 0;
+    int enforcou = 0;
+    char chutesdousuario[26];
+    int tentativas = 0;
+
+    do {
+        
+        for (int i = 0; i < strlen(palavrasecreta); i++){
+            
+            int achou = 0;
+
+            for (int j = 0; j < tentativas; j++) {
+                if(chutesdousuario[j] == palavrasecreta[i]){
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if (achou) {
+                printf("%c ", palavrasecreta[i]);
+            } else {            
+                printf("_ ");
+            }
+        }
+        printf("\n");
+        char chute;
+        printf("Qual letra? ");
+        scanf(" %c", &chute);    
+
+
+
+        chutesdousuario[tentativas] = chute;
+        tentativas++;
+
+
+
+    } while (!acertou && !enforcou);
+    
+    
+    
+    
 }
